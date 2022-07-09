4e3b183a8e4e384de81dd66f84ed045e224b31d1
Chidi Jude <cjude@appzonegroup.com>
2022-07-09 12:04:34 +0100

    added and installed EF, and did migration and seed products

--- BlazorEcommerce/Server/Program.cs
+++ BlazorEcommerce/Server/Program.cs
@@ -1,10 +1,14 @@
 global using BlazorEcommerce.Shared;
-using Microsoft.AspNetCore.ResponseCompression;
+global using Microsoft.EntityFrameworkCore;
+global using BlazorEcommerce.Server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddEndpointsApiExplorer();
